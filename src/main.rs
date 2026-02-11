e65735c2860121e67eced7171d7f20e931876380
Tomás Esteves <tomasesteves2002@gmail.com>
2026-02-11 17:19:55 +0000

    feat: add display and test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bytes::Buf;
+use core::fmt;
 use std::{collections::HashMap, env, io::Error, net::UdpSocket, vec};
 
 use rand::Rng;
@@ -128,6 +129,26 @@ impl Header {
     }
 }
 
+impl fmt::Display for Header {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "ID: {}", self.id)?;
+
+        writeln!(f, "QR: {}", self.flags >> 15)?;
+        writeln!(f, "OPCODE: {}", self.flags >> 11 & 0x7)?;
+        writeln!(f, "AA: {}", self.flags >> 10 & 0x1)?;
+        writeln!(f, "TC: {}", self.flags >> 9 & 0x1)?;
+        writeln!(f, "RD: {}", self.flags >> 8 & 0x1)?;
+        writeln!(f, "RA: {}", self.flags >> 7 & 0x1)?;
+        writeln!(f, "Z: {}", self.flags >> 4 & 0x7)?;
+        writeln!(f, "RCODE: {}", self.flags & 0xF)?;
+
+        writeln!(f, "QDCOUNT: {}", self.qdcount)?;
+        writeln!(f, "ANCOUNT: {}", self.ancount)?;
+        writeln!(f, "NSCOUNT: {}", self.nscount)?;
+        writeln!(f, "ARCOUNT: {}", self.arcount)
+    }
+}
+
 impl DomainName {
     fn from_str(domain: &str) -> Self {
         let labels = domain.split('.').map(|s| s.to_string()).collect();
@@ -141,6 +162,16 @@ impl DomainName {
         }
 
         buf.push(0); // terminator
+    }
+}
+
+impl fmt::Display for DomainName {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for label in &self.labels {
+            write!(f, "{} ", label)?;
+        }
+
+        Ok(())
     }
 }
 
@@ -268,6 +299,14 @@ impl Question {
     }
 }
 
+impl fmt::Display for Question {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "QNAME: {}", self.qname)?;
+        writeln!(f, "QTYPE: {:?}", self.qtype)?;
+        writeln!(f, "QCLASS: {:?}", self.qclass)
+    }
+}
+
 impl Answer {
     fn from_bytes(buf: &mut &[u8], len: usize, labels: &mut HashMap<usize, DomainName>) -> Self {
         let mut name = String::new();
@@ -333,6 +372,19 @@ impl Answer {
     }
 }
 
+impl fmt::Display for Answer {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "NAME: {}", self.name)?;
+        writeln!(f, "TYPE: {:?}", self.rtype)?;
+        writeln!(f, "CLASS: {:?}", self.class)?;
+        writeln!(f, "TTL: {}", self.ttl)?;
+        writeln!(f, "RDLENGTH: {}", self.rdlength)?;
+        writeln!(f, "RDATA: {:?}", self.rdata)?;
+
+        Ok(())
+    }
+}
+
 impl Message {
     fn create_query(domain: &str, qtype: &str, qclass: &str) -> Self {
         Self {
@@ -393,6 +445,37 @@ impl Message {
     }
 }
 
+impl fmt::Display for Message {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "##### MESSAGE #####")?;
+
+        writeln!(f, "### HEADER ###")?;
+        writeln!(f, "{}", self.header)?;
+
+        for q in &self.question {
+            writeln!(f, "### QUESTION ###")?;
+            writeln!(f, "{q}")?;
+        }
+
+        for a in &self.answer {
+            writeln!(f, "### ANSWER ###")?;
+            writeln!(f, "{a}")?;
+        }
+
+        for a in &self.authority {
+            writeln!(f, "### AUTHORITY ###")?;
+            writeln!(f, "{a}")?;
+        }
+
+        for a in &self.additional {
+            writeln!(f, "### ADDITIONAL ###")?;
+            writeln!(f, "{a}")?;
+        }
+
+        Ok(())
+    }
+}
+
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 4 {
@@ -415,7 +498,31 @@ fn main() -> std::io::Result<()> {
     let mut data = &buf[..len];
 
     let response = Message::from_bytes(&mut data);
-    println!("{:#?}", response);
+    println!("{}", response);
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use dns_parser::QueryType;
+
+    use super::*;
+
+    #[test]
+    fn create_query() {
+        let domain = "tomase.pt";
+        let qtype = "A";
+        let qclass = "IN";
+
+        let packet = Message::create_query(domain, qtype, qclass);
+
+        let mut builder = dns_parser::Builder::new_query(packet.header.id, true);
+
+        builder.add_question(domain, false, QueryType::A, dns_parser::QueryClass::IN);
+
+        let reference = builder.build().unwrap();
+
+        assert_eq!(packet.to_bytes(), reference)
+    }
 }
