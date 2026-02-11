564e2f1bdbb382794a497d325cdd702bfd5f2d70
Tomás Esteves <tomasesteves2002@gmail.com>
2026-02-11 17:42:30 +0000

    feat: add nix app

--- flake.nix
+++ flake.nix
@@ -2,15 +2,32 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
+    rust-overlay,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+
+      rustToolchain = pkgs.rust-bin.stable.latest.default;
+
+      myRustApp = pkgs.rustPlatform.buildRustPackage {
+        pname = "dns_client";
+        version = "0.1.0";
+        src = ./.;
+
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+      };
     in {
       devShell = pkgs.mkShell {
         buildInputs = with pkgs; [
@@ -19,11 +36,18 @@
           rustfmt
           rust-analyzer
           clippy
+          rustToolchain
         ];
 
         shellHook = ''
           export RUST_LOG=debug
         '';
+      };
+
+      packages.default = myRustApp;
+
+      apps.default = flake-utils.lib.mkApp {
+        drv = myRustApp;
       };
     });
 }
